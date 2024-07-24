5f5657d064a660d95e7e9ecb2d0c6e6837dc92d3
5enox <animikantan@gmail.com>
2024-07-24 11:30:08 +0100

    adadw

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import configparser
 import requests
 from pathlib import Path
 from extract_data import get_data
-from get_cookies import get_cookies
+# from get_cookies import get_cookies
 
 # Configure logging
 logging.basicConfig(level=logging.INFO,
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-cookies = get_cookies()
+# cookies = get_cookies()
 
 
 def transform_url(url):
@@ -26,10 +26,19 @@ def transform_url(url):
 def fetch_gallery_data(url):
     headers = {key: config["LISTING_HEADERS"].get(
         key, '') for key in config["LISTING_HEADERS"]}
-    with open('cookie.txt', 'r') as file:
-        cookie = file.read().strip()
-    headers["Cookie"] = cookie
-    response = requests.get(url, headers=headers, cookies=cookies)
+    headers["Cookie"] = ("OptanonConsent=groups%3DC0004%3A0; visid_incap_2011533=/oIGRmwESOOOCHCTcLiH++BVnmYAAAAAQkIPAAAAAAC8jmZkX2dOepDP9CkIjeRE; "
+                         "visid_incap_984766=OtrnOEBeSxet7Hf9Y+1afhlanmYAAAAAQkIPAAAAAACA8Ny1AcRA2okPhMp8A0itk/fqO8WbXPFy; "
+                         "nlbi_984766=0um0M2KRgTNTTyukoeynngAAAAAvd6uzKc4PUOOV8G2IWPKV; "
+                         "incap_ses_250_984766=6tF3K2bcj1CNV2mK1C14A2mqoGYAAAAAxpSfaAecRf5cABYJ7L56ug==; "
+                         "incap_ses_505_984766=x3VAGavj9hiKp59R8R4CB0TKoGYAAAAAG+aBVXOc2aIXQj3wKAKlXQ==; "
+                         "AT=MTY4NTE1MTozOmE5dFdEZ1lGOGRh; DI=null; AI=null; U=583028766669e6026853a7d793f36; "
+                         "laravel_token=eyJpdiI6IjgzakVSVEp4YmliZGdPanNNQmR4eFE9PSIsInZhbHVlIjoiUTliakRBQjVFY2RYczd1cldyV1hNNEFaejhZdkFqdWtsL3I5eWphZllQNSt1RThnWDRRd3VPbXpqT1VHRDVBOVhBU3hBMW1XOWtDYXowZEEzejh3MWtqcUtNNlVIS0tqdjloWTBGNUkyai9yajlLU1BMQnRWaFZKakhieUNHS0FXYXpUY2xUY05BbzYrWjBObnNkSFVPWUgwa01aUVdSZVI3aXlTTDk4QSthVWVYWmxDQ0JUVmRNR3dJRWdqc2pnYjkxWjh2YVNxa0lYSFQ4bmtRWkU3VzRTdHNVRWVvNDBkelQ1VG9RdWREalVYOVF2Q00vWFdDeFM5TWJ5anNHT01Udm5ZNFAzYitScy9PV0FScVBEcDY3YWkxMGdneXJVQWp2VGdXVUpvemgwcFRhQkFqUTRkQmJFOEc0YXRiTlAiLCJtYWMiOiI1Njc3NzY1MGMwZWJmMWIwZmI3OGVhYmM0NzIzZjIwM2I0ZDkyZDdlY2FiNDYwOWYyNWQ2YjI1NTM0MzMzZjBiIiwidGFnIjoiIn0%3D; "
+                         "XSRF-TOKEN=eyJpdiI6IjFpOWNCQ2dWT1UrdjBtYjQwVVhFUVE9PSIsInZhbHVlIjoiWkpsNFcrYWdhTTBkd3FmLzZmOG9sZXF0eEdFQ1A5amRkVnNtZGRHUEd3Y2o2U1Z1am8wcVF2Lzc5L2dUODlrSTNyLzZGYjdKQm5VUGlNWUtVRmcyb0piT0M0OFNHc1BxKzVQU05mb0FLbSt4U0VZSVlOL2hla09TdVdIcUVmVU4iLCJtYWMiOiJiYTFjNmUxNjU0ZGQxOGM5YmJmOTVhYzdiNGEwZGU4ZGViYzY2YWFmM2Y1NjhhZGFlMmQ4Njg2M2M4MmJjYTBmIiwidGFnIjoiIn0%3D; "
+                         "hemmings_session=eyJpdiI6InpDWnFyR0JYM01SMW8vY2w2MkFSUVE9PSIsInZhbHVlIjoiQk9lb2g0V0tVZ000MU9nVlhQaUtCbnVrSXhjYmxhK25oaEdQSHE4aWhPVW1YQStXKzVDS3VaM2Q3em56a3hYenBjTlQ1TlNXSXRuaWN4SG96QkRNTklQVXFKVFgxVnAreXNDR2h2RjNRTWhDaHM2b2grRmdSRkhDOWtuVnlxUWEiLCJtYWMiOiJmZTgwYTEwZDJlZDhlZDQyYzNlMjkyMDg5MWEyOGJjMTZkNTZlZmQ3ZmZkM2ZkYzNkMzQ5N2Y4MTAxMTBiOWE0IiwidGFnIjoiIn0%3D; "
+                         "AWSALB=41fvJfgqCdoCv5Hds+jo/4IjObbrbFMVY6MUFEm4HXS3uHYkJoJU5Dg5ad38GyCemDa5O+rslfiHf+0YmouZaA1ncFFzL5MnTgaIR8ImaHPtq7aHkjSWK4D8ZVnShJJeu0fX/UNpYwp91442E2wgSVTBjAy6Aaq7U/RMH+fgL2mb/VX014V+YQVKFWa2paGsyuMAnx25qjexjLjTrx3zHK/SvUik8ST15sgG3V9HHPS8WtvwM8g9vAGvLZbUXck=; "
+                         "AWSALBCORS=41fvJfgqCdoCv5Hds+jo/4IjObbrbFMVY6MUFEm4HXS3uHYkJoJU5Dg5ad38GyCemDa5O+rslfiHf+0YmouZaA1ncFFzL5MnTgaIR8ImaHPtq7aHkjSWK4D8ZVnShJJeu0fX/UNpYwp91442E2wgSVTBjAy6Aaq7U/RMH+fgL2mb/VX014V+YQVKFWa2paGsyuMAnx25qjexjLjTrx3zHK/SvUik8ST15sgG3V9HHPS8WtvwM8g9vAGvLZbUXck=")
+
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
