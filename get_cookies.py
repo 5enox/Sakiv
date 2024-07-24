6bd623c8ab412ba980a951d56a576e448b198184
5enox <animikantan@gmail.com>
2024-07-24 11:04:40 +0100

    niuce

--- get_cookies.py
+++ get_cookies.py
@@ -12,7 +12,7 @@ PASSWORD = config["LOGIN"]["password"]
 
 def get_cookies():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 
